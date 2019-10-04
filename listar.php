2aa91f7950a566529e53432bc41407d1153af1b8
Nycolas-Guia <nycolas.guia@hotmail.com>
2019-10-04 19:55:02 -0300

    adicionando função de listar

---
+++ listar.php
@@ -0,0 +1,25 @@
+<?php
+$conecta = mysqli_connect("localhost","id11034010_crudusuario","crudusuario","id11034010_crudusuario");
+
+
+$query = "SELECT * FROM tb_usuario ORDER by nm_usuario";
+
+result = mysqli_query($conecta,$query);
+
+$registros = array(
+	'pessoas' => array();
+	);
+
+$i = 0;
+
+while($linha = mysqli_fetch_assoc($result)){
+	$registros['pessoas'][$i] = array(
+		'codigo'=>$linha['cd_usuario'],
+		'nome'=>$linha['nm_usuario'],
+		'email'=>$linha['ds_email'],
+		'senha'=>$linha['ds_senha']
+	);
+	$i++;
+}
+
+echo json_encode(registros);
